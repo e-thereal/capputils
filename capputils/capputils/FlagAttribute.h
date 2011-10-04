89ceb37d05f8b4188925ae392a99de23d04691a0
brosch.tom <brosch.tom@gmail.com>
2011-10-04 21:08:21 +0000

    Documentation updated.

--- capputils/capputils/FlagAttribute.h
+++ capputils/capputils/FlagAttribute.h
@@ -1,12 +1,13 @@
-/*
- * Flag.h
+/**
+ * \brief Contains the flag attribute
+ * \file Flag.h
  *
- *  Created on: Feb 10, 2011
- *      Author: tombr
+ * \date Feb 10, 2011
+ * \author Tom Brosch
  */
 
-#ifndef FLAG_H_
-#define FLAG_H_
+#ifndef CAPPUTILS_FLAG_H_
+#define CAPPUTILS_FLAG_H_
 
 #include "IAttribute.h"
 
@@ -14,16 +15,36 @@ namespace capputils {
 
 namespace attributes {
 
+/**
+ * \brief Makes a class property a boolean flag which is either set or not.
+ *
+ * This attribute changes the parsing behavior of the \c ArgumentParser. The according
+ * parameter switch does not require any succeeding arguments. If the switch is detected,
+ * the according property is set to true.
+ */
 class FlagAttribute: public virtual IAttribute {
 public:
+  /**
+   * \brief Constructs a new \c FlagAttribute
+   *
+   * Use the Flag() function which returns a wrapped FlagAttribute when passing
+   * this attribute to an argument list.
+   */
   FlagAttribute();
+
+  /**
+   * \brief Destructs the flag attribute
+   */
   virtual ~FlagAttribute();
 };
 
+/**
+ * \brief Returns a wrapped \c FlagAttribute
+ */
 AttributeWrapper* Flag();
 
 }
 
 }
 
-#endif /* FLAG_H_ */
+#endif /* CAPPUTILS_FLAG_H_ */
