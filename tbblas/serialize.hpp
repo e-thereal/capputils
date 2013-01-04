0cf3fa7bba5d3234afde89ebcc6c12986bda11f5
brosch.tom <brosch.tom@gmail.com>
2013-01-04 19:29:40 +0000

    Tensor serialization now also saves the fullsize parameter of the tensor. This breaks compatibility with previous tensor serialization implementations.

--- tbblas/serialize.hpp
+++ tbblas/serialize.hpp
@@ -16,9 +16,11 @@ namespace tbblas {
 
 template<class T, unsigned dim, bool device>
 void serialize(const tbblas::tensor<T, dim, device>& tensor, std::ostream& out) {
-  // TODO: save fullsize here. This will break compatibility with previous tensor serialization
   typename tbblas::tensor<T, dim, device>::dim_t size = tensor.size();
   out.write((char*)&size, sizeof(size));
+
+  typename tbblas::tensor<T, dim, device>::dim_t fullsize = tensor.fullsize();
+  out.write((char*)&fullsize, sizeof(fullsize));
 
   std::vector<T> buffer(tensor.count());
   thrust::copy(tensor.begin(), tensor.end(), buffer.begin());
@@ -33,10 +35,10 @@ void serialize(const tbblas::tensor<T, dim, device>& tensor, const std::string& 
 
 template<class T, unsigned dim, bool device>
 void deserialize(std::istream& in, tbblas::tensor<T, dim, device>& tensor) {
-  // TODO: save fullsize here. This will break compatibility with previous tensor serialization
-  typename tbblas::tensor<T, dim, device>::dim_t size;
+  typename tbblas::tensor<T, dim, device>::dim_t size, fullsize;
   in.read((char*)&size, sizeof(size));
-  tensor.resize(size, size);
+  in.read((char*)&fullsize, sizeof(fullsize));
+  tensor.resize(size, fullsize);
 
   std::vector<T> buffer(tensor.count());
   in.read((char*)&buffer[0], sizeof(T) * tensor.count());
