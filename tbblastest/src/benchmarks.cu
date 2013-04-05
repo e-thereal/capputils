ca16c6d0e4e6c2ab885f4f4a47c25d78ddc9765f
brosch.tom <brosch.tom@gmail.com>
2013-04-05 22:56:07 +0000

    NIPS performance tests added.

--- tbblastest/src/benchmarks.cu
+++ tbblastest/src/benchmarks.cu
@@ -8,12 +8,14 @@
 #include "tests.h"
 
 #include <tbblas/tensor.hpp>
+#include <tbblas/io.hpp>
 #include <tbblas/fft.hpp>
 #include <tbblas/math.hpp>
 #include <tbblas/random.hpp>
 #include <tbblas/filter.hpp>
 #include <tbblas/filter2.hpp>
-#include <tbblas/io.hpp>
+#include <tbblas/dot.hpp>
+#include <tbblas/mask.hpp>
 
 #include <boost/timer.hpp>
 
@@ -35,10 +37,27 @@ typedef tbblas::random_tensor<value_t, 4, true, tbblas::uniform<value_t> > randu
 void benchmarks() {
   using namespace tbblas;
 
-  boost::timer _timer;
+#if 0
+  randu_t randu(32, 32, 32, 64), randKernel(5, 5, 5, 64);
+  tensor_t A = randu, B, C1, C2, C3, kernel = randKernel;
+
+  tensor_t::dim_t topleft = kernel.size() / 2, paddedSize = A.size() + kernel.size() - 1;
+  topleft[3] = 0;
+  paddedSize[3] = A.size()[3];
+
+  tensor_t A2 = zeros<value_t>(paddedSize);
+  A2[topleft, A.size()] = A;
+  B = filter(A2, flip(flip(flip(kernel, 0), 1), 2), 3);
+  C1 = B[topleft, A.size()];
+  C2 = filter3d(A, kernel, naive());
+  C3 = filter3d(A, kernel, optimized());
+  tbblas_print(dot(C1 - C2, C1 - C2));
+  tbblas_print(dot(C1 - C3, C1 - C3));
+  tbblas_print(dot(C2 - C3, C2 - C3));
+#endif
 
 #if 0
-  tensor<value_t, 3, true> A(4, 4, 1), A2 = zeros<value_t>(6, 6, 3), B, C, kernel(3, 3, 1);
+  tensor<value_t, 3, true> A(4, 4, 1), A2 = zeros<value_t>(6, 6, 3), B, C, C2, kernel(3, 3, 1);
   A = 1, 2, 3, 4,
       5, 4, 3, 2,
       1, 2, 5, 6,
@@ -51,74 +70,103 @@ void benchmarks() {
       -2, 4, -2,
       -1, 2, -1;
 
-  B = filter3d(A, kernel);
-  C = filter(A2, flip(kernel));
+  B = filter3d(A, kernel, optimized());
+  C = filter3d(A, kernel, naive());
+  C2 = filter(A2, flip(kernel));
 
   tbblas_print(A);
   tbblas_print(kernel);
   tbblas_print(B);
   tbblas_print(C);
-//  tbblas_print(C[seq(1,1,1), B.size()]);
+  tbblas_print(C2[seq(1,1,1), B.size()]);
+  tbblas_print(dot(B - C, B - C));
+  tbblas_print(repeat(A, seq(1,1,2)));
 #endif
 
 #if 1
-  randu_t randu(32, 32, 32, 64), randKernel(5, 5, 5, 64);
+  boost::timer _timer;
 
-  tensor_t A = randu, B = randu, C = randu, S, kernel = randKernel, T;
+  const int iterations = 1000;
+
+  randu_t randv(32, 32, 32, 64), randF(5, 5, 5, 64), randh(32, 32, 32, 1);
+
+  tensor_t A = randv, B = randv, C = randv, C2, S = randh, kernel = randF, T;
   ctensor_t cA = fft(A, 3), cB = fft(B, 3), cC = fft(C, 3), cS, cT;
   STOP("Init")
 
   // Addition
-  TIMER_LOOP(1000) C = A + B;
+  TIMER_LOOP(iterations) C = A + B;
   STOP("Addition")
-  TIMER_LOOP(1000) cC = cA + cB;
+  TIMER_LOOP(iterations) cC = cA + cB;
   STOP("Addition")
 
   // Multiplication
-  TIMER_LOOP(1000) C = A * B;
+  TIMER_LOOP(iterations) C = A * B;
   STOP("Multiplication")
-  TIMER_LOOP(1000) cC = cA * cB;
+  TIMER_LOOP(iterations) cC = cA * cB;
   STOP("Multiplication")
 
   // Complex conjugate
-  TIMER_LOOP(1000) cC = conj(cA);
+  TIMER_LOOP(iterations) cC = conj(cA);
   STOP("Complex conjugate ")
-  TIMER_LOOP(1000) cC = conj(cA) * cB;
+  TIMER_LOOP(iterations) cC = conj(cA) * cB;
   STOP("Times complex conjugate ")
 
   // fft, ifft
   fft_plan<4> plan, iplan;
-  TIMER_LOOP(1000) cA = fft(A, 3);
+  TIMER_LOOP(iterations) cA = fft(A, 3);
   STOP("FFT")
-  TIMER_LOOP(1000) cA = fft(A, 3, plan);
+  TIMER_LOOP(iterations) cA = fft(A, 3, plan);
   STOP("FFT")
-  TIMER_LOOP(1000) A = ifft(cA, 3);
+  TIMER_LOOP(iterations) A = ifft(cA, 3);
   STOP("iFFT")
-  TIMER_LOOP(1000) A = ifft(cA, 3, plan);
+  TIMER_LOOP(iterations) A = ifft(cA, 3, plan);
   STOP("iFFT")
 
+  // filtering
+  TIMER_LOOP(iterations) C = filter(A, kernel, 3);
+  STOP("filter")
+  TIMER_LOOP(iterations) C = filter(A, flip(kernel), 3);
+  STOP("filter")
+//  TIMER_LOOP(iterations) C = filter3d(A, kernel, naive());
+//  STOP("filter")
+  TIMER_LOOP(iterations) C = filter3d(A, kernel, optimized());
+  STOP("filter")
+
   // Sum
-  TIMER_LOOP(1000) cS = sum(cA, 3);
+  TIMER_LOOP(iterations) cS = sum(cA, 3);
   STOP("sum")
 
   // fft, ifft of a 'slice'
-  TIMER_LOOP(1000) S = ifft(cS, 3);
+  TIMER_LOOP(iterations) S = ifft(cS, 3);
   STOP("iFFT(slice)")
-  TIMER_LOOP(1000) S = ifft(cS, 3, plan);
+  TIMER_LOOP(iterations) S = ifft(cS, 3, plan);
   STOP("iFFT(slice)")
-  TIMER_LOOP(1000) cS = fft(S, 3);
+  TIMER_LOOP(iterations) cS = fft(S, 3);
   STOP("FFT(slice)")
-  TIMER_LOOP(1000) cS = fft(S, 3, plan);
+  TIMER_LOOP(iterations) cS = fft(S, 3, plan);
   STOP("FFT(slice)")
 
   // nrelu_mean
-  TIMER_LOOP(1000) T = S = nrelu_mean(S);
+  TIMER_LOOP(iterations) T = S = nrelu_mean(S);
   STOP("nrelu_mean")
 
-  // filtering
-  TIMER_LOOP(1000) C = filter(A, kernel, 3);
-  STOP("filter")
-  TIMER_LOOP(1000) C = filter3d(A, kernel);
-  STOP("filter")
+  TIMER_LOOP(iterations) cC = conj(cB) * cA;
+  STOP("inverse filter")
+  TIMER_LOOP(iterations) cC = 0.01 * conj(cB) * cA;
+  STOP("inverse filter")
+  TIMER_LOOP(iterations) cC = cC + 0.01 * conj(cB) * cA;
+  STOP("inverse filter")
+  TIMER_LOOP(iterations) cC = cC + 0.01 * repeat(conj(cS), cA.size() / cS.size()) * cA;
+  STOP("inverse filter")
+  TIMER_LOOP(iterations) kernel = filter(A, C, kernel.size(), 3);
+  STOP("inverse filter")
+  TIMER_LOOP(iterations) kernel = filter(A, repeat(flip(S), A.size() / S.size()), kernel.size(), 3);
+  STOP("inverse filter")
+
+//  C = filter3d(A, kernel, naive());
+//  C2 = filter3d(A, kernel, optimized());
+//  tbblas_print(dot(C - C2, C - C2));
+
 #endif
 }
